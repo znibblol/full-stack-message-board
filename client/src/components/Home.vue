594e544fbde6a7a231b70b911e8ffe7739396518
Emil Malm <emilamalm@gmail.com>
2020-06-28 22:07:32 +0200

    Fixed mobile bug, fixed required on the right places

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -6,10 +6,9 @@
       <li class="media py-3" v-for="message in messages" :key="message._id">
         <img :src="message.imageUrl" class="mr-3" :alt="message.subject" :title="message.subject" v-if="message.imageUrl">
         <div class="media-body">
-          <p class="mt-0 mb-1 font-italic">{{ message.username }}</p>
+          <p class="mt-0 mb-1 font-italic">{{ message.username }} <span class="text-muted small">{{ message.created }}</span></p>
           <h5 class="mt-0 mb-1">{{ message.subject }}</h5>
           <p>{{ message.message }}</p>
-          <small>{{message.created}}</small>
         </div>
       </li>
     </ul>
@@ -17,16 +16,16 @@
     <div class="add-form col-sm-2 bg-secondary">
       <form>
         <div class="form-group">
-          <label for="username">Username <span class="required">*</span></label>
-          <input type="text" name="username" id="username" class="form-control" value="Anonymous" required>
+          <label for="username">Username</label>
+          <input type="text" name="username" id="username" class="form-control" value="Anonymous">
         </div>
         <div class="form-group">
           <label for="subject">Subject <span class="required">*</span></label>
           <input type="text" name="subject" id="subject" class="form-control" required>
         </div>
         <div class="form-group">
-          <label for="message">Message</label>
-          <textarea v-model="messageLength" @keyup="charCount()" name="message" id="message" rows="3" class="form-control"></textarea>
+          <label for="message">Message <span class="required">*</span></label>
+          <textarea v-model="messageLength" @keyup="charCount()" name="message" id="message" rows="3" class="form-control" required></textarea>
           <span class="text-muted small">{{ totChar }} / 500</span>
         </div>
         <div class="form-group">
@@ -91,15 +90,19 @@ li {
   font-size: 2rem;
 }
 .add-form {
-  /* background: #FFF; */
-  border: 1px solid #FFF;
   border-radius: 15px;
   box-shadow: 1px 1px 10px rgba(0,0,0,0.5);
   padding: 2rem 1rem;
   position: fixed;
-  right: -350px;
+  right: -500px;
   top: 10%;
   transition: all 200ms ease;
+}
+@media screen and (max-width: 600px) {
+  .add-form {
+    width: 250px;
+    top: 17%;
+  }
 }
 .open {
   right: 10px;
