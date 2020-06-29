714aa3a21656f4139973f17a4e0987eeb17c7c5d
Emil Malm <emilamalm@gmail.com>
2020-06-29 23:10:36 +0200

    npm run build, and deleted 'apa' from routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,7 @@
 import Home from './components/Home'
-import Apa from './components/Apa'
 
 const routes =[
     { path: '/', component: Home },
-    { path: '/apa', component: Apa },
 ];
 
 export default routes;
