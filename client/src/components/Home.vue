f7c14cae46a7bdf0056fdb7885b8b2fad60bc071
Emil Malm <emilamalm@gmail.com>
2020-06-29 21:33:08 +0200

    maybe final commit before deploy

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -3,7 +3,7 @@
     <button class="btn btn-primary btn-add" @click="showForm()"><span class="fa fa-plus"></span></button>
     <h1 class="text-center">The message board</h1>
     <ul class="list-unstyled">
-      <li class="media py-3" v-for="message in messages" :key="message._id">
+      <li class="media py-3" v-for="message in reversedMessages" :key="message._id">
         <img :src="message.imageUrl" class="mr-3" :alt="message.subject" :title="message.subject" v-if="message.imageUrl">
         <div class="media-body">
           <p class="mt-0 mb-1 font-italic">{{ message.username }} <span class="text-muted small">{{ message.created }}</span></p>
@@ -17,20 +17,28 @@
       <form @submit.prevent="addMessage">
         <div class="form-group">
           <label for="username">Username</label>
-          <input type="text" name="username" id="username" class="form-control" value="Anonymous">
+          <input type="text" name="username" id="username" class="form-control" v-model="message.username">
         </div>
         <div class="form-group">
           <label for="subject">Subject <span class="required">*</span></label>
-          <input type="text" name="subject" id="subject" class="form-control" required>
+          <input type="text" name="subject" id="subject" class="form-control" v-model="message.subject" required>
         </div>
         <div class="form-group">
           <label for="message">Message <span class="required">*</span></label>
-          <textarea v-model="messageLength" @keyup="charCount()" name="message" id="message" rows="3" class="form-control" required></textarea>
+          <textarea v-model="message.message" @keyup="charCount()" name="message" id="message" rows="3" class="form-control" required></textarea>
           <span class="text-muted small">{{ totChar }} / 500</span>
         </div>
         <div class="form-group">
           <label for="imageUrl">Image URL</label>
-          <input type="url" name="imageUrl" id="imageUrl" class="form-control">
+          <input type="url" name="imageUrl" id="imageUrl" class="form-control" v-model="message.imageUrl">
+        </div>
+        <div class="alert alert-dismissible alert-danger" v-if="error">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <strong>Oh snap!</strong> {{ error }}
+        </div>
+        <div class="alert alert-dismissible alert-success" v-if="success">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <strong>Well done!</strong> You have posted a message on the greates message board on this planet!
         </div>
         <button type="submit" class="btn btn-primary btn-block">Add message</button>
       </form>
@@ -46,8 +54,15 @@ export default {
   name: 'Home',
   data: () => ({
     messages: [],
-    messageLength: "",
     totChar: 0,
+    error: '',
+    success: false,
+    message: {
+      username: 'Anonymous',
+      subject: '',
+      message: '',
+      imageUrl: '',
+    },
   }),
   mounted() {
     fetch(API_URL).then(response => response.json()).then(result => {
@@ -59,10 +74,40 @@ export default {
       $('.add-form').toggleClass('open');
     },
     charCount() {
-      this.totChar = this.messageLength.length;
+      this.totChar = this.message.message.length;
     },
     addMessage() {
-      
+      console.log(this.message);
+      fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(this.message),
+        headers: {
+          'content-type': 'application/json',
+        },
+      }).then(response => response.json()).then(result => {
+        if(result.details) {
+          // there was an error
+          const error = result.details.map(detail => detail.message).join(' ');
+          // console.log(error);
+          console.log(result);
+          this.error = error;
+          this.success = false;
+        } else {
+          // console.log(result);
+          this.messages.push(result);
+          this.success = true;
+          this.error = '';
+          this.message.username = 'Anonymous';
+          this.message.subject = '';
+          this.message.message = '';
+          this.message.imageUrl = '';
+        }
+      });
+    }
+  },
+  computed: {
+    reversedMessages() {
+      return this.messages.slice().reverse();
     }
   },
 }
